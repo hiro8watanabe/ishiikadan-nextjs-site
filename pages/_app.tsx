28dfbaeba8664f1cd0bb0023962afc561e04d9c6
hiro <onecast.onelost615@gmail.com>
2023-05-12 15:34:34 +0900

    マウスストーカー追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,15 @@ import "../styles/reset.css";
 import "../styles/debug.scss";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { MouseStalker } from "../components/MouseStalker/MouseStalker";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MouseStalker/>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
